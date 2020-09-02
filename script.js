22648445a825ad96ba94c3ab108d3fe000a05764
Pavlov <NPavlov@forshtadt.ru>
2020-09-02 12:23:20 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+a = 5;
+console.log(a);
+
+var number = 5;
+var string = 'Hello!';
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log('fgdfgdf'*9);
+
+let something;
+console.log(something);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+
+console.log(persone.age);
+
+// alert('asdadasd');
+
+// let answer = confirm('AAAAAAA');
+// console.log(answer)
+
+let answer = prompt('SDSDSD?','Yes');
+console.log(typeof(answer));
