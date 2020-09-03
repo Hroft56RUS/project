b149ba1f98cb30995336bd50a265cd4e3ca3ccc7
Pavlov <NPavlov@forshtadt.ru>
2020-09-03 10:33:45 +0500

    final

--- script.js
+++ script.js
@@ -13,17 +13,32 @@ let appData = {
     };
 
 for (let i = 0; i < 2; i++) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце",""),
-        b = prompt("Во сколько обойдется?","");
-    
-    if ( (typeof(a))==='sring' && (typeof(a) != null) && (typeof(b) != null) && (a != "") && (b != "") 
-         && (a.length < 50) ) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце",''),
+        b = +prompt("Во сколько обойдется?",'');
+
+    if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
         console.log("done");
         appData.expenses[a] = b;
     } else {
-        continue;
+        console.log("bad result");
+        i--;
     }
+};
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Произошла ошибка");
 }
+
 
 //let i = 0;
 // while (i < 2) {
@@ -35,7 +50,8 @@ for (let i = 0; i < 2; i++) {
 //         console.log("done");
 //         appData.expenses[a] = b;
 //     } else {
-//         continue;
+//         console.log("bad result");
+//           i--;
 //     }
 //     i++;
 // }
@@ -49,12 +65,11 @@ for (let i = 0; i < 2; i++) {
 //         console.log("done");
 //         appData.expenses[a] = b;
 //     } else {
-//         continue;
+//         console.log("bad result");
+//           i--;
 //     }
 //     i++;
 // } while (i < 2);
 
-
-alert(appData.budget/30);
 
 
