c46d071ebdf87aa2a0f9481997e63d675d224ba0
Pavlov <NPavlov@forshtadt.ru>
2020-09-10 16:39:48 +0500

    lesson yoga

--- Lesson_6/js.js
+++ Lesson_6/js.js
@@ -1,39 +1,6 @@
-let btn = document.querySelectorAll("button"),
-    wrap = document.querySelector('.wrapper'),
-    link = document.querySelector('a');
+let timerId = setTimeout(sayHello, 3000);
+clearTimeout(timerId);
 
-// btn[0].onclick = function() {
-//     alert("Click 1");
-//     alert("Click AAAA 1");
-// };
-
-// btn[0].onclick = function() {
-//     alert("Click AAAA 1");
-// };
-
-// btn[0].addEventListener('click',function(event){
-//     console.log('Произошло событие: ' + event.type + ' на элементе ' + event.target);
-// });
-
-// wrap.addEventListener('click',function() {
-//     console.log('Произошло событие: ' + event.type + ' на элементе ' + event.target);
-// });
-
-// link.addEventListener('click',function(event){
-//     event.preventDefault();
-//     console.log('Произошло событие: ' + event.type + ' на элементе ' + event.target);
-// });
-
-
-btn.forEach(function(item){
-    item.addEventListener('mouseleave', function(){
-        console.log('Exit');
-    });
-});
-// btn[0].addEventListener('click',function(){
-//     alert("Click AAAA 1");
-// });
-
-// btn[0].addEventListener('mouseenter',function(){
-//     alert("Click BBBBBB 1");
-// });
+function sayHello(){
+    alert('dfsdfsdf');
+}
