9e6b7cbcc69fcf1fbe83379389728e54b0ad2452
Pavlov <NPavlov@forshtadt.ru>
2020-09-14 16:09:53 +0500

    timer

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -1,11 +1,12 @@
-window.addEventListener('DOMContentLoaded',function(){
+'use strict';
 
-    'use strict';
+window.addEventListener('DOMContentLoaded',ClickOnTab('info-header','info-header-tab','info-tabcontent'));
 
-    let tab = document.querySelectorAll('.info-header-tab'),
-        info = document.querySelector('.info-header'),
-        tabContent = document.querySelectorAll('.info-tabcontent');
-    
+function ClickOnTab(mainTab,childTab,contentTab) {
+    let tab = document.querySelectorAll('.' + childTab),
+        info = document.querySelector('.' + mainTab),
+        tabContent = document.querySelectorAll('.' + contentTab);
+
     function hideTabContent(a) {
         for (let i = a; i < tabContent.length; i++) {
             tabContent[i].classList.remove('show');
@@ -24,7 +25,8 @@ window.addEventListener('DOMContentLoaded',function(){
 
     info.addEventListener('click', function(event) {
         let target = event.target;
-        if (target && target.classList.contains('info-header-tab')) {
+
+        if (target && target.classList.contains(childTab)) {
             for (let i = 0; i < tab.length; i++) {
                 if (target == tab[i]) {
                     hideTabContent(0);
@@ -34,4 +36,52 @@ window.addEventListener('DOMContentLoaded',function(){
             }
         }
     });
-});
+
+    // Таймер
+    let deadLine = '2020-09-15';
+
+    function getTimeRemaining(endTime) {
+        let t = Date.parse(endTime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes': minutes,
+            'seconds' : seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock,1000);
+        
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+
+            function addZero(num) {
+                if (num <= 9) {
+                    return num = '0' + num;
+                } else return num;
+            }
+
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = "00";
+                minutes.textContent = "00";
+                seconds.textContent = "00";
+            }
+        }
+    }
+
+    setClock('timer',deadLine);
+}
+
